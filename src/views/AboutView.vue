173a230c160a725c64983a512fca9742dae5f2d3
Olli2881 <122098154+Olli2881@users.noreply.github.com>
2023-10-16 12:14:10 +0000

    fyrir heima

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,65 @@
+<script>
+
+export default {
+  data() {
+    return {
+      name: " Óli Viðar",
+      movies: ["Blu beatle", "deadpool", "spider-man: no way home"]
+    }
+  }
+}
+</script>
+
+<style>
+html, body{
+  height: 100%;
+}
+
+body {
+  display: grid;
+  background-image: url("");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.about{
+  display: flex;
+  justify-content: center;
+  padding: 15px;
+  margin-left: 50px;
+  margin-right: 50px;
+  background-color: darkblue;
+  border-radius: 6px;
+}
+
+main{
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  text-align: left;
+}
+
+li{
+  margin-bottom: 15px;
+  padding: 35px;
+  width: 100%;
+  background-color: white;
+  border-radius: 6px;
+}
+
+</style>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+
+    <main>
+      Væntanlegt
+    </main>
+
+    <ul>
+      <li v-for="movie in movies">{{ movie }}</li>
+    </ul>
+
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,78 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+
+export default {
+  data() {
+    return {
+      name: " Óli Viðar",
+      movies: ["Blu beatle", "deadpool", "spider-man: no way home"]
+    }
+  }
+}
 </script>
 
+<style>
+html, body{
+  height: 100%;
+}
+
+body {
+  display: grid;
+  background-image: url("");
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: darkblue;
+}
+
+.home{
+  display: flex;
+  justify-content: center;
+  padding: 15px;
+  margin-left: 50px;
+  margin-right: 50px;
+  background-color: #8B0000;
+  border-radius: 6px;
+}
+
+main{
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  text-align: left;
+  color: white;
+}
+
+li{
+  margin-bottom: 15px;
+  padding: 25px;
+  width: 100%;
+  background-color: white;
+  border-radius: 6px;
+}
+
+</style>
+
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+
+  <div class="home">
+
+    <main>
+      Heima síða
+    </main>
+
+    <ul>
+      <li v-for="movie in movies">{{ movie }}</li>
+    </ul>
+
+  </div>
+
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
